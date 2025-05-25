182e6eebb35ed8bc96f141d93e79e7e8ca98ee88
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2025-05-25 22:17:26 +0530

    implemented useState for counter

--- counter_frontend/src/App.jsx
+++ counter_frontend/src/App.jsx
@@ -1,33 +1,39 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const increaseValue = () => {
+    const newValue = value + 1;
+    setValue(newValue);
+    console.log(`New counted value: ${newValue}`);
+  };
+
+  const decreaseValue = () => {
+    if (value > 0) {
+      return setValue(value - 1);
+    } else {
+      console.log("Cannot decrease value beyond 0!");
+    }
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Practising counter using useState hook!</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <hr />
+      <div className="value">Counter Value: {value}</div>
+      <div className="buttonsdiv">
+        <span>
+          <button onClick={() => increaseValue()}>Increase Value</button>
+        </span>
+        <span>
+          <button onClick={() => decreaseValue()}>Decrease Value</button>
+        </span>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div></div>
     </>
   );
 }
